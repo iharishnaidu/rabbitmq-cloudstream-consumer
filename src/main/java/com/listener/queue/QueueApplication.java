e7bf7c52b91f046611279aff3dbda4234f6b8cdb
cah-harish-andey <harish.andey@cardinalhealth.com>
2020-11-06 11:21:36 -0500

    base code

---
+++ src/main/java/com/listener/queue/QueueApplication.java
@@ -0,0 +1,22 @@
+package com.listener.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+@SpringBootApplication
+public class QueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueApplication.class, args);
+	}
+
+	@StreamListener(target = Sink.INPUT)
+	public void titansAssembling(String titan) {
+		System.out.println("Titans Assembled " + titan);
+	}
+
+}
